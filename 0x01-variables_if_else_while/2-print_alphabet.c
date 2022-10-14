f4013d4200645c73db6a5d2e4e303d8410285b96
Biyahon <biyahon@gmail.com>
2022-10-14 13:39:28 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= '2')
+			putchar(c)
+			c++;
+          	}
+	putchar('\n");
+	return (0);
+}
+
