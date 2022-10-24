cb70e422b6e3be24de2faee8e17f43bea2d13d3b
Biyahon <biyahon@gmail.com>
2022-10-24 05:38:23 -0700

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
  * _puts - prints a string
  * @str: string to be printed
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str++);
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
