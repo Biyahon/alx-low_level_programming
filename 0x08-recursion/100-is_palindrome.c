b30aff56674ca13b71845e9ad9484008dea7676c
Biyahon <biyahon@gmail.com>
2022-11-02 08:19:52 -0700

    task8

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * find_strlen - Returns the length of a string
+ * @s: the string to be measured
+ * Return: the length of the string
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
+}
