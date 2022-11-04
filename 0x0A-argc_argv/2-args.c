e84fe4a8588e61a8e80700ac958424b48fb5536a
Biyahon <biyahon@gmail.com>
2022-11-04 06:34:38 -0700

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,14 +10,13 @@
  */
 int main(int argc, char *argv[])
 {
-
+	/*declaring variables*/
 	int count = 0;
 
 	if (argc > 0)
 	{
+		/*WHILE - print each arguments*/
 		while (count < argc)
-	}
+		{
 	printf("%s\n", argv[count]);
-		return (0);
-}
 
