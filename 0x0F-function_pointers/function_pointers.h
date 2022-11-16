41bb2818066a1f2d6f285ebb0683157a1e615b56
Biyahon <biyahon@gmail.com>
2022-11-16 05:52:04 -0800

    task0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define _FUNCTION_POINTERS_
 #include <stdlib.h>
 
+int _putchar(char *c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
